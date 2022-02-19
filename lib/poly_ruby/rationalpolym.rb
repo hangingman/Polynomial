875f6451308faa24bce551beff2bfe5e11a99f5e
hiroyuki.nagata <idiotpanzer@gmail.com>
2022-02-19 17:27:59 +0900

    Add rationalpoly_m_spec(RationalPolyM)

--- lib/poly_ruby/rationalpolym.rb
+++ lib/poly_ruby/rationalpolym.rb
@@ -1,3 +1,4 @@
+# coding: euc-jp
 # RationalPolyM
 #
 ############################################
@@ -5,7 +6,7 @@
 #
 # Based on rational.rb by Keiju ISHITSUKA(SHL Japan Inc.)
 #
-# This module is distributed freely in the sence of 
+# This module is distributed freely in the sence of
 # Ruby's License.
 ############################################
 # --
@@ -13,7 +14,7 @@
 #   class RationalPolyM
 #
 #   RationalPolyM(a, b) or RPolyM(a, b) --> a/b
-#       a,b admit Numeric,PolynomialM 
+#       a,b admit Numeric,PolynomialM
 #       and String of polynomial.
 #   +, -, *, /, **, %, divmod
 #   abs
@@ -34,8 +35,7 @@
 #   <=>
 #   reduce
 
-require "polynomialm"
-require "rational"
+require "poly_ruby/polynomialm"
 
 def RationalPolyM(a, b = PolynomialM(1))
 	if a.kind_of?(RationalPolyM)
@@ -119,7 +119,7 @@ def + (a)
 		return x + y
 	end
 end
-  
+
 def - (a)
 	return self+(-a)
 end
@@ -142,7 +142,7 @@ def * (a)
 		return x * y
 	end
 end
-  
+
 def / (a)
 	if a.kind_of?(RationalPolyM)
 		if a.zero?; raise ZeroDivisionError, "denometor is 0" ;end
@@ -164,7 +164,7 @@ def / (a)
 		return x / y
 	end
 end
-  
+
 def ** (other)
 	if other.kind_of?(Integer)
 		if other > 0
@@ -243,7 +243,7 @@ def to_poly
 	q,r=@numerator.divmod(@denominator)
 	return q
 end
-  
+
 
 def to_s(format="text")
     case format
