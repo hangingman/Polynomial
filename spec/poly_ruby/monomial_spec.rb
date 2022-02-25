7063daf9afd63d4eabc985a1ba85e0f7277f5c39
hiroyuki.nagata <idiotpanzer@gmail.com>
2022-02-25 23:42:03 +0900

    Add Monomial specs

--- spec/poly_ruby/monomial_spec.rb
+++ spec/poly_ruby/monomial_spec.rb
@@ -11,6 +11,37 @@ RSpec.describe Monomial do
   it "can convert integer to monomial" do
     expect(Monomial(c=2, p={"x"=>0}).to_s).to eq "2"
   end
+  it "can get/set term order" do
+    Monomial.set_term_order("lex")
+    expect(Monomial.get_term_order).to eq "lex"
+    Monomial.set_term_order("deglex")
+    expect(Monomial.get_term_order).to eq "deglex"
+    Monomial.set_term_order("degrevlex")
+    expect(Monomial.get_term_order).to eq "degrevlex"
+  end
+
+  describe "#power_product" do
+    it "power_product(3*x^2) = x^2" do
+      expect(Monomial(c=3, p={"x"=>2}).power_product.to_s).to eq "x^(2)"
+    end
+    it "power_product(9*x^4) = x^4" do
+      expect(Monomial(c=9, p={"x"=>4}).power_product.to_s).to eq "x^(4)"
+    end
+  end
+
+  describe "#lcm, #gcd" do
+    context "Given y^5, y^2" do
+      m1 = Monomial(c=1, p={"y"=>5})
+      m2 = Monomial(c=1, p={"y"=>2})
+
+      it "lcm(y^5, y^2) = y^5" do
+        expect(m1.lcm(m2).to_s).to eq m1.to_s
+      end
+      it "gcd(y^5, y^2) = y^2" do
+        expect(m1.gcd(m2).to_s).to eq m2.to_s
+      end
+    end
+  end
 
   describe "#to_s(format)" do
     context "Given 5x^4" do
