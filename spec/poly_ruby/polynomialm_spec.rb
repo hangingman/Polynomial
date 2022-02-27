bc3c2e4c00884b9e2e31038fe8c2a49e9fafbe09
hiroyuki.nagata <idiotpanzer@gmail.com>
2022-02-27 22:03:02 +0900

    Add Grobner basis spec files

--- spec/poly_ruby/polynomialm_spec.rb
+++ spec/poly_ruby/polynomialm_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require "poly_ruby/polynomialm" # multi variable polynomial class
 require "poly_ruby/gbasem" # Rational, Complex or Z/pZ coefficients Grobner basis
 require "poly_ruby/gbasei" # Z coefficient Grobner basis
@@ -42,7 +43,6 @@ RSpec.describe PolynomialM do
       f5=f3.substitute("x"=>2,"y"=> 1)
       expect(f5.to_s).to eq "3"
     end
-
     # We can use substitute method to polynomials.
     # In the following sample, We exchange x and y each other.
     it "f(x,y) = x+y^(2); f(y,x) = x^2+y" do
@@ -58,78 +58,93 @@ RSpec.describe PolynomialM do
       f2=f1.derivative(["x"])
       expect(f2.to_s).to eq "2x+5"
     end
-    it "f(x) = (2x-3)^4; d/dx f(x) = 8(2x-3)^3" do
+    skip "f(x) = (2x-3)^4; d/dx f(x) = 8(2x-3)^3" do
       f1=PolynomialM("(2x-3)^4")
       f2=f1.derivative(["x"])
       #expect(f2.to_s).to eq ""
       #expect(f2.to_s).to eq PolynomialM("8*(2*x-3)**3").to_s
     end
-    it "" do
+    skip "f(y) = (y-1)^3; d/dy f(x) = " do
       #print "-- derivative\n"
       #f2=PolynomialM("(y-1)^3")
       #f6=f2.derivative(["x","x","y"])
       #printf 'f2.derivative(["x","x","y"])=%s'+"\n",f6
     end
   end
-  it "" do
-    #
-    # print "-- integral\n"
-    # f7=f3.integral(["x","y"])
-    # printf 'f3.integral(["x","y"])=%s'+"\n",f7
-  end
-
-  skip "" do
-    f = PolynomialM("x+y^2").substitute("x" => PolynomialM("y"), "y" => PolynomialM("x"))
-    print f, "\n"
+  describe "#integral" do
+    it "f(x,y) = x+y^2; ∫ f(x,y) dx dy = x*y^(3)/3+x^(2)*y/2" do
+      f3=PolynomialM("x+y^2")
+      f7=f3.integral(["x","y"])
+      expect(f7.to_s).to eq "x*y^(3)/3+x^(2)*y/2"
+    end
   end
 end
 
 
 RSpec.describe GBase do
-  skip "skip" do
-    print "---- sample 1 ---\n"
-    print "-- solve equation using Grobner basis\n"
-    f1=PolynomialM("x+y+2z-2")
-    f2=PolynomialM("2x+3y+6z-5")
-    f3=PolynomialM("3x+2y+4z-5")
-    printf "%s =0, %s =0, %s =0\n",f1,f2,f3
-    gbasis=GBase.getGBase([f1,f2,f3])
-    printf "Groebner baseis: %s\n", gbasis.join(", ")
-    str="";d=""; gbasis.each{|f| str=str+d+f.to_s+" =0"; d=", "}
-    printf "Solution: %s\n", str
 
-    print "---- sample 2 ---\n"
-    print "-- Grobner basis in degrevlex order \n"
-    f1=PolynomialM("3x^2y-y*z")
-    f2=PolynomialM("x*y^2+z^4")
-    printf "%s, %s\n",f1,f2
-    gbasis=GBase.getGBase([f1,f2])
-    printf "Groebner base: %s\n", gbasis.join(", ")
+  describe "#getGBase; sample1: solve equation using Grobner basis" do
+    context "f1=x+y+2z-2, f2=2x+3y+6z-5, f3=3x+2y+4z-5" do
+      f1=PolynomialM("x+y+2z-2")
+      f2=PolynomialM("2x+3y+6z-5")
+      f3=PolynomialM("3x+2y+4z-5")
 
-    print "-- with deglex term order\n"
-    # Monomial.set_term_order(t)
-    #	 t= "lex"(default), "deglex",  "degrevlex"
-    Monomial.set_term_order("deglex")
-    gbasis=GBase.getGBase([f1,f2])
-    printf "Groebner basis: %s\n", gbasis.join(", ")
-    Monomial.set_term_order
+      it do
+        gbasis=GBase.getGBase([f1,f2,f3])
+        expect(gbasis.map(&:to_s)).to match ["x-1/1", "y+2z-1"]
+      end
+    end
+  end
 
-    print "---- sample 3 ---\n"
-    print "-- Grobnae basis in Z/5Z coefficients. \n"
-    f1=PolynomialM("x^2+y^2+1")
-    f2=PolynomialM("x^2y+2x*y+x")
-    printf "%s, %s\n",f1,f2
-    gbasis=GBase.getGBaseZp([f1,f2],5)
-    printf "Groebner basis: %s\n", gbasis.join(", ")
+  describe "#getGBase; sample2: Grobner basis" do
 
-    print "--- sample 4 ---\n"
-    print "-- Grobner basis in Z coefficnents. \n"
-    Monomial.set_var_order
-    Monomial.set_term_order("lex")
-    f1=PolynomialM("6x^2+y^2")
-    f2=PolynomialM("10x^2y+2x*y")
-    printf "%s, %s\n",f1,f2
-    gbasis=GBaseI.getGBaseI([f1,f2])
-    printf "Groebner basis: %s\n", gbasis.join(", ")
+    context "f1=3x^2y-y*z, f2=x*y^2+z^4" do
+      f1=PolynomialM("3x^2y-y*z")
+      f2=PolynomialM("x*y^2+z^4")
+
+      context "with lex term order" do
+        it do
+          Monomial.set_term_order("lex")
+          gbasis=GBase.getGBase([f1,f2])
+          expect(gbasis.map(&:to_s)).to match ["x^(2)*y-y*z/3", "x*y^(2)+z^(4)", "x*z^(4)+y^(2)*z/3", "y^(4)*z-3z^(8)"]
+        end
+      end
+      context "with deglex term order" do
+        it do
+          Monomial.set_term_order("deglex")
+          gbasis=GBase.getGBase([f1,f2])
+          expect(gbasis.map(&:to_s)).to match ["z^(4)+x*y^(2)", "x^(2)*y-y*z/3"]
+        end
+      end
+    end
+  end
+
+  describe "#getGBaseZp; sample3: Grobner basis in Z/5Z coefficients" do
+    context "f1=x+y+2z-2, f2=2x+3y+6z-5, f3=3x+2y+4z-5" do
+      f1=PolynomialM("x^2+y^2+1")
+      f2=PolynomialM("x^2y+2x*y+x")
+
+      it do
+        gbasis=GBase.getGBaseZp([f1,f2],5)
+        expect(gbasis.map(&:to_s)).to match ["y^(3)+3x*y+4x+y", "x^(2)+y^(2)+1"]
+      end
+    end
+  end
+
+  describe "#getGBaseI; sample4: Grobner basis in Z coefficnents" do
+    context "f1=6x^2+y^2, f2=10x^2y+2x*y" do
+      Monomial.set_var_order
+      Monomial.set_term_order("lex")
+      f1=PolynomialM("6x^2+y^2")
+      f2=PolynomialM("10x^2y+2x*y")
+
+      it do
+        gbasis=GBaseI.getGBaseI([f1,f2])
+        expect(gbasis.map(&:to_s)).to match ["x^(2)*y^(3)+y^(5)+4x*y^(3)+y^(3)",
+                                             "2x^(2)*y+2y^(3)-2x*y",
+                                             "5y^(3)-6x*y",
+                                             "6x^(2)+y^(2)"]
+      end
+    end
   end
 end
